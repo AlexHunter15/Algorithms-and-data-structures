1d555d51cb83d730512dbde58f41e50439724652
AlexHunter15 <134641085+AlexHunter15@users.noreply.github.com>
2023-10-26 18:22:16 +0300

    Add files via upload

---
+++ Алгоритмы на графах/AISD0.2(GRAF).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    int n;
+    in >> n;
+
+    vector<int> arr(n, 0); 
+
+    for (int i = 1; i < n; i++) {
+        int u, v;
+        in >> u >> v;
+        arr[v - 1] = u;
+    }
+
+    for (int i = 0; i < n; i++) {
+        out << arr[i] << " ";
+    }
+    out << endl;
+
+    in.close();
+    out.close();
+
+    return 0;
+}
