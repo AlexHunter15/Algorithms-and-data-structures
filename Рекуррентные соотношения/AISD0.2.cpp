6ddfad49b9a7be9a487cb822f67b1568c62168a3
AlexHunter15 <134641085+AlexHunter15@users.noreply.github.com>
2023-10-26 17:09:06 +0300

    Add files via upload

---
+++ Рекуррентные соотношения/AISD0.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+const int MOD = 1000000007;
+int C[1001][1001];
+int main() {
+    int n, k;
+    scanf_s("%d %d", &n, &k);
+    for (int i = 0; i <= n; i++) {
+        C[i][0] = 1;
+        C[i][i] = 1;
+    }
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % MOD;
+        }
+    }
+    printf("%d\n", C[n][k]);
+    return 0;
+}
+
+
+
+
+
+
