86822bf84f883db8872ed23b4b70e3cb9dae2640
AlexHunter15 <134641085+AlexHunter15@users.noreply.github.com>
2023-10-26 17:51:21 +0300

    Add files via upload

---
+++ Специальные структуры данных/AISD0.2(STRUCT).cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+bool isHeap(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = n - 1; i > 0; i--) {
+        int p = (i - 1) / 2;
+        if (arr[i] < arr[p]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    int n;
+    in >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        in >> arr[i];
+    }
+
+    if (isHeap(arr)) {
+        out << "Yes" << endl;
+    }
+    else {
+        out << "No" << endl;
+    }
+
+    in.close();
+    out.close();
+
+    return 0;
+}
