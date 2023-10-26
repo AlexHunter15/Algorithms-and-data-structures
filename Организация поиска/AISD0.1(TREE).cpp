52d0af3196037bf20f1892fd4b8c47a9c2038633
AlexHunter15 <134641085+AlexHunter15@users.noreply.github.com>
2023-10-26 17:29:17 +0300

    Add files via upload

---
+++ Организация поиска/AISD0.1(TREE).cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+struct TreeNode {
+    int key;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val) : key(val), left(nullptr), right(nullptr) {}
+};
+
+TreeNode* insert(TreeNode* root, int key) {
+    if (root == nullptr) {
+        return new TreeNode(key);
+    }
+
+    if (key < root->key) {
+        root->left = insert(root->left, key);
+    }
+    else if (key > root->key) {
+        root->right = insert(root->right, key);
+    }
+
+    return root;
+}
+
+void leftBP(TreeNode* root, ofstream& outFile) {
+    if (root == nullptr) {
+        return;
+    }
+
+    outFile << root->key << endl;
+    leftBP(root->left, outFile);
+    leftBP(root->right, outFile);
+}
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    TreeNode* root = nullptr;
+    int key;
+    set<int> k;
+
+    while (in >> key) {
+        if (k.find(key) == k.end()) {
+            root = insert(root, key);
+            k.insert(key);
+        }
+    }
+
+    leftBP(root, out);
+
+    in.close();
+    out.close();
+
+    return 0;
+}
