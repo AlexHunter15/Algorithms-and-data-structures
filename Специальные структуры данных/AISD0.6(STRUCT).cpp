39b3d3284f9db37463e7df687567f11ccd132414
AlexHunter15 <134641085+AlexHunter15@users.noreply.github.com>
2023-11-09 21:31:22 +0300

    Add files via upload

---
+++ Специальные структуры данных/AISD0.6(STRUCT).cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+const int empty = -1;
+
+int hash(int x, int m, int c, int i) {
+    return ((x % m) + c * i) % m;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::ifstream in("input.txt");
+    std::ofstream out("output.txt");
+
+    int m, c, n;
+    in >> m >> c >> n;
+
+    std::vector<int> a(m, empty);
+
+    for (int j = 0; j < n; j++) {
+        int x;
+        in >> x;
+        int i = 0;
+        int ind;
+        do {
+            ind = hash(x, m, c, i);
+            if (a[ind] == x || a[ind] == empty) {
+                break;
+            }
+            i++;
+        } while (true);
+        a[ind] = x;
+    }
+
+    in.close();
+
+    for (int i = 0; i < m; ++i) {
+        out << a[i] << " ";
+    }
+
+    out.close();
+
+    return 0;
+}
